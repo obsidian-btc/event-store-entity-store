b656c808afda81916329f389ca867059de83cd6f
Scott Bellware <sbellware@gmail.com>
2015-08-10 16:59:06 -0500

    Entity controls use the entity controls in the projection library

--- lib/event_store/entity_store/controls/entity.rb
+++ lib/event_store/entity_store/controls/entity.rb
@@ -1,7 +1,15 @@
 module EventStore
   module EntityStore
     module Controls
-      Entity = EventStore::EntityProjection::Controls::Entity
+      module Entity
+        def self.example
+          EventStore::EntityProjection::Controls::Entity.example
+        end
+
+        def self.new
+          EventStore::EntityProjection::Controls::Entity::SomeEntity.new
+        end
+      end
     end
   end
 end
