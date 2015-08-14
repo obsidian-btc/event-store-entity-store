9412a964c90c4cb11296dafb4795c22f6421f63f
Scott Bellware <sbellware@gmail.com>
2015-08-14 17:14:31 -0500

    Entity example has attribute data

--- lib/event_store/entity_store/controls/entity.rb
+++ lib/event_store/entity_store/controls/entity.rb
@@ -3,7 +3,12 @@ module EventStore
     module Controls
       module Entity
         def self.example
-          EventStore::EntityProjection::Controls::Entity.example
+          entity = EventStore::EntityProjection::Controls::Entity.example
+
+          entity.some_attribute = Controls::Message.attribute
+          entity.some_time = Controls::Message.time
+
+          entity
         end
 
         def self.entity_class
