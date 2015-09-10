0de0f0708ae48fa513512c3ed0688dc1fd1c8ef1
Scott Bellware <sbellware@gmail.com>
2015-09-09 22:12:16 -0500

    Testing of the immediate refresh strategy is more complete

--- test/spec/store/refresh_policy/immediate.rb
+++ test/spec/store/refresh_policy/immediate.rb
@@ -2,25 +2,42 @@ require_relative '../store_init'
 
 describe "Immediate Cache Refresh Policy" do
   stream_name = EventStore::EntityStore::Controls::Writer.write_batch 'someEntity'
-
   id = EventStore::EntityStore::Controls::StreamName.id(stream_name)
-
-  cache = EventStore::EntityStore::Cache::Factory.build_cache :some_subject
-
-  entity = EventStore::EntityStore::Controls::Entity.new
-  entity.some_attribute = EventStore::EntityStore::Controls::Message.attribute
-
-  cache.put id, entity, 0
-
   projection_class = EventStore::EntityStore::Controls::Projection::SomeProjection
-
   refresh = EventStore::EntityStore::Cache::RefreshPolicy::Immediate
-  refresh.! id, cache, projection_class, stream_name, entity.class
 
-  record = cache.get id
+  describe "The entity is previously cached" do
+    cache = EventStore::EntityStore::Cache::Factory.build_cache :some_subject
 
-  specify "Projects the entity" do
+    entity = EventStore::EntityStore::Controls::Entity.new
+    cache.put id, entity, 0
+
+    refresh.! id, cache, projection_class, stream_name, entity.class
+
+    record = cache.get id
     entity = record.entity
-    refute(entity.nil?)
+
+    describe "Updates the cached entity" do
+      specify "some_attribute is not projected due as the cached version is 0" do
+        assert(entity.some_attribute.nil?)
+      end
+
+      specify "some_time is projected" do
+        refute(entity.some_time.nil?)
+      end
+    end
+  end
+
+  describe "The entity is not previously cached" do
+    cache = EventStore::EntityStore::Cache::Factory.build_cache :some_subject
+
+    refresh.! id, cache, projection_class, stream_name, EventStore::EntityStore::Controls::Entity
+
+    record = cache.get id
+    entity = record.entity
+
+    specify "Caches the entity" do
+      refute(entity.nil?)
+    end
   end
 end
