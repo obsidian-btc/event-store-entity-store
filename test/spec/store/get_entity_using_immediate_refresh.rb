993b3b662e26efe770220da1e94c9229b7e2ac9b
Scott Bellware <sbellware@gmail.com>
2015-09-18 12:39:18 -0500

    Test uses the store's cache, rather than creating an independant cache object

--- test/spec/store/get_entity_using_immediate_refresh.rb
+++ test/spec/store/get_entity_using_immediate_refresh.rb
@@ -3,18 +3,18 @@ require_relative './store_init'
 describe "Get Entity Using the Immediate Refresh Policy" do
   stream_name = EventStore::EntityStore::Controls::Writer.write_batch 'someEntity'
 
+  store = EventStore::EntityStore::Controls::Store::SomeStore.build refresh: :immediate
+  category_name = stream_name.split('-')[0]
+  store.category_name = category_name
+
   id = EventStore::EntityStore::Controls::StreamName.id(stream_name)
 
-  cache = EventStore::EntityStore::Cache::Factory.build_cache :some_subject
+  cache = store.cache
 
   entity = EventStore::EntityStore::Controls::Entity.new
   entity.some_attribute = EventStore::EntityStore::Controls::Message.attribute
 
   initial_cache_record = cache.put id, entity, 0
-
-  store = EventStore::EntityStore::Controls::Store::SomeStore.build
-  category_name = stream_name.split('-')[0]
-  store.category_name = category_name
 
   entity, version, time = store.get id, include: [:version, :time]
 
