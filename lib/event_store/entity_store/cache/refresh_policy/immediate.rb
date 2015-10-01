ec34cffd38f50d323022a441a9fcf08f99c3c819
Scott Bellware <sbellware@gmail.com>
2015-10-01 00:45:02 -0500

    Immediate's actuator is generalized

--- lib/event_store/entity_store/cache/refresh_policy/immediate.rb
+++ lib/event_store/entity_store/cache/refresh_policy/immediate.rb
@@ -5,26 +5,56 @@ module EventStore
         module Immediate
           include RefreshPolicy
 
-          def self.!(id, cache, projection_class, stream_name, entity_class)
+          def self.refresh(id, cache, projection_class, stream_name, entity_class, &blk)
             logger.trace "Refreshing (ID: #{id}, Stream Name: #{stream_name}, Projection Class: #{projection_class}, Entity Class: #{entity_class})"
 
             cache_record = cache.get(id)
 
-            unless cache_record
-              entity = new_entity(entity_class)
-              starting_position = nil
-            else
-              entity = cache_record.entity
-              starting_position = cache_record.version + 1
-            end
-
-            cache_record = update_cache(entity, id, cache, projection_class, stream_name, starting_position)
+            cache_record = blk.(cache_record)
 
             logger.trace "Refreshed (ID: #{id}, Stream Name: #{stream_name}, Projection Class: #{projection_class}, Entity Class: #{entity_class})"
             logger.data "Cache Record: #{cache_record.inspect}"
 
             cache_record
           end
+
+          def self.!(id, cache, projection_class, stream_name, entity_class)
+            cache_record = refresh(id, cache, projection_class, stream_name, entity_class) do |cache_record|
+              unless cache_record
+                entity = new_entity(entity_class)
+                starting_position = nil
+              else
+                entity = cache_record.entity
+                starting_position = cache_record.version + 1
+              end
+
+              cache_record = update_cache(entity, id, cache, projection_class, stream_name, starting_position)
+              cache_record
+            end
+
+            cache_record
+          end
+
+          # def self.!(id, cache, projection_class, stream_name, entity_class)
+          #   logger.trace "Refreshing (ID: #{id}, Stream Name: #{stream_name}, Projection Class: #{projection_class}, Entity Class: #{entity_class})"
+
+          #   cache_record = cache.get(id)
+
+          #   unless cache_record
+          #     entity = new_entity(entity_class)
+          #     starting_position = nil
+          #   else
+          #     entity = cache_record.entity
+          #     starting_position = cache_record.version + 1
+          #   end
+
+          #   cache_record = update_cache(entity, id, cache, projection_class, stream_name, starting_position)
+
+          #   logger.trace "Refreshed (ID: #{id}, Stream Name: #{stream_name}, Projection Class: #{projection_class}, Entity Class: #{entity_class})"
+          #   logger.data "Cache Record: #{cache_record.inspect}"
+
+          #   cache_record
+          # end
 
           def self.logger
             @logger ||= Telemetry::Logger.get self
