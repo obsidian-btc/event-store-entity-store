8602bc14154be777406fa449f0efce3e93ba5765
Scott Bellware <sbellware@gmail.com>
2015-11-05 16:46:46 -0600

    Requiring entity_projection loads the subordinate dependencies

--- lib/event_store/entity_store.rb
+++ lib/event_store/entity_store.rb
@@ -1,9 +1,3 @@
-require 'identifier/uuid'
-require 'set_attributes'
-require 'schema'
-require 'telemetry/logger'
-require 'dependency'; Dependency.activate
-require 'clock'
 require 'event_store/entity_projection'
 
 require 'event_store/entity_store/cache/refresh_policy'
