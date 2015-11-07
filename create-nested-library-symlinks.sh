544b12aa296034e4df02bf91a8ead76f7d52fca5
Scott Bellware <sbellware@gmail.com>
2015-11-07 15:23:03 -0600

    Factored nested symlink creation into an external script

---
+++ create-nested-library-symlinks.sh
@@ -0,0 +1,43 @@
+set -e
+
+if [ -z ${LIBRARIES_DIR+x} ]; then
+  echo "LIBRARIES_DIR must be set to the libraries directory path... exiting"
+  exit 1
+fi
+
+if [ ! -d "$LIBRARIES_DIR" ]; then
+  echo "$LIBRARIES_DIR does not exit... exiting"
+  exit 1
+fi
+
+function symlink-nested-library {
+  directory=$1
+  name=$2
+
+  full_name=$directory/$name
+  dest="$LIBRARIES_DIR/$directory"
+
+  if [ ! -d "$dest" ]; then
+    mkdir -p "$dest"
+  fi
+
+  echo
+  echo "Symlinking $full_name"
+  echo "- - -"
+
+  if [ -h "$dest/$name" ]; then
+    for entry in $LIBRARIES_DIR/$full_name*; do
+      if [ -e "$entry" ]; then
+        echo "- removing symlink: $entry"
+        rm $entry
+      fi
+    done
+  fi
+
+  echo "- creating symlinks"
+  ln -s $(PWD)/lib/$full_name* $dest
+
+  echo "- - -"
+  echo "($full_name done)"
+  echo
+}
