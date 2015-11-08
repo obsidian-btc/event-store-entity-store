b321e7abfc51111e5783fb92d69e6bea74d89bc5
Scott Bellware <sbellware@gmail.com>
2015-11-07 19:22:27 -0600

    Nested library symlinks script can handle nested and regular libraries

--- create-nested-library-symlinks.sh
+++ create-nested-library-symlinks.sh
@@ -14,30 +14,72 @@ function symlink-nested-library {
   directory=$1
   name=$2
 
-  full_name=$directory/$name
+  echo
+  echo "Symlinking $name"
+  echo "- - -"
+
   dest="$LIBRARIES_DIR/$directory"
 
   if [ ! -d "$dest" ]; then
+    echo "Making directory $dest"
     mkdir -p "$dest"
   fi
 
+  symlink-library $name $directory
+}
+
+function symlink-library {
+  name=$1
+  directory=$2
+
   echo
-  echo "Symlinking $full_name"
+  echo "Symlinking $name"
   echo "- - -"
 
-  if [ -h "$dest/$name" ]; then
-    for entry in $LIBRARIES_DIR/$full_name*; do
-      if [ -e "$entry" ]; then
-        echo "- removing symlink: $entry"
-        rm $entry
-      fi
-    done
+  src="$(PWD)/lib"
+  dest="$LIBRARIES_DIR"
+  if [ ! -z "$directory" ]; then
+    src="$src/$directory"
+    dest="$dest/$directory"
   fi
+  src="$src/$name"
 
-  echo "- creating symlinks"
-  ln -s $(PWD)/lib/$full_name* $dest
+  echo "Destination is $dest"
+
+  full_name=$directory/$name
+
+  for entry in $src*; do
+    entry_basename=$(basename $entry)
+    dest_item="$dest/$entry_basename"
+
+    if [ -h "$dest_item" ]; then
+      echo "- removing symlink: $dest_item"
+      rm $dest_item
+    fi
+
+    echo "- symlinking $entry_basename to $dest_item"
+
+    cmd="ln -s $entry $dest_item"
+    echo $cmd
+    ($cmd)
+
+    echo
+  done
+
+
+  # if [ -h "$dest/$name" ]; then
+  #   for entry in $LIBRARIES_DIR/$full_name*; do
+  #     if [ -e "$entry" ]; then
+  #       echo "- removing symlink: $entry"
+  #       rm $entry
+  #     fi
+  #   done
+  # fi
+
+  # echo "- creating symlinks"
+  # ln -s $(PWD)/lib/$full_name* $dest
 
   echo "- - -"
-  echo "($full_name done)"
+  echo "($name done)"
   echo
 }
