8c76f9bbc4b2b12d7d80f4227334a4f102167af1
Nathan Ladd <nathan@obsidianexchange.com>
2016-03-07 13:30:54 -0600

    Remove nil argument

--- lib/event_store/entity_store/cache/refresh_policy/missing.rb
+++ lib/event_store/entity_store/cache/refresh_policy/missing.rb
@@ -9,7 +9,7 @@ module EventStore
             refresh(id, cache, projection_class, stream_name, entity_class) do |cache_record|
               unless cache_record
                 entity = new_entity(entity_class)
-                cache_record = update_cache(entity, id, cache, projection_class, stream_name, nil, session: session)
+                cache_record = update_cache(entity, id, cache, projection_class, stream_name, session: session)
                 cache_record
               end
             end
