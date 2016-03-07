df78ae420520439e99cb68e944a62a5f89e817b2
Nathan Ladd <nathan@obsidianexchange.com>
2016-03-07 13:07:36 -0600

    Move temporary "do not modify" message to bench.rb

--- test/bench.rb
+++ test/bench.rb
@@ -1,5 +1,9 @@
 require_relative 'test_init'
 
+at_exit do
+  fail "Do not further elaborate on this library until we break out the refresh policies into classes that can be dependencies; the design in general needs a thorough review" # [Nathan Ladd, Mon Mar 7 2016]
+end
+
 TestBench::Runner.(
   'bench/**/*.rb',
   exclude_pattern: %r{/skip\.|(?:_init\.rb|\.sketch\.rb|_sketch\.rb|\.skip\.rb)\z}
