57403a85f320cd76bd05bbd1a8eb302f3108fa03
Scott Bellware <sbellware@gmail.com>
2016-03-08 09:17:04 -0600

    Library closure announcement uses logging rather than an error

--- test/bench.rb
+++ test/bench.rb
@@ -1,7 +1,22 @@
 require_relative 'test_init'
 
 at_exit do
-  fail "Do not further elaborate on this library until we break out the refresh policies into classes that can be dependencies; the design in general needs a thorough review" # [Nathan Ladd, Mon Mar 7 2016]
+  ENV['LOG_LEVEL'] = 'fubar'
+  ENV['LOGGER'] = 'on'
+  ENV['LOG_METADATA'] = 'off'
+  logger = Telemetry::Logger.get __FILE__
+  logger.debug " "
+  logger.warn "WARNING: event_store-entity_store is closed for elaboration"
+  logger.debug " "
+  logger.fail "This library is scheduled for an overhaul. Elaboration of this library will cause that work to have longer delays and greater cost."
+  logger.debug " "
+  logger.fubar "Issues:"
+  logger.fubar "- Reify refresh policies as classes, rather than modules"
+  logger.fubar "- Understand lifecycle and object allocation issues"
+  logger.fubar "- Review design ethos in its entirety"
+  logger.fubar "[Nathan Ladd, Mon Mar 7 2016]"
+  logger.fubar "[Scott Bellware, Tue Mar 8 2016]"
+  logger.debug " "
 end
 
 TestBench::Runner.(
